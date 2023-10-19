7ee21b1443bbceb1615c1d3cbfc81630bcc45064
kaneki881 <you@example.com>
2023-10-19 09:12:35 -0700

    :)

--- push.c
+++ push.c
@@ -7,12 +7,31 @@
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-	stack_t* newNode = (stack_t*)malloc(sizeof(stack_t));
-	if (newNode == NULL) {
+	stack_t *new_node;
+	char *arg = strtok(NULL, " \t\n");
+	int value;
+
+	if (arg == NULL || !isdigit(*arg))
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	value = atoi(arg);
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-	newNode->n = line_number;
-	newNode->next = *stack;
-	*stack = newNode;
+
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack != NULL)
+		(*stack)->prev = new_node;
+
+	*stack = new_node;
 }
